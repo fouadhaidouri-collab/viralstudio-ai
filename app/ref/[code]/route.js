38573beb295312afa39f1fd6dee8a2b48c637669
fouadhaidouri-collab <fouadhaidouri@gmail.com>
2026-07-03 11:33:14 +0100

    Simplify ref route to isolate error

--- app/ref/[code]/route.js
+++ app/ref/[code]/route.js
@@ -1,22 +1,21 @@
 import { NextResponse } from "next/server";
-import { get, run } from "../../../lib/db";
-import { recordClick } from "../../../lib/affiliateStore";
+import { get } from "../../../lib/db";
 
 export async function GET(req, { params }) {
   try {
     const { code } = await params;
     const affiliate = await get("SELECT * FROM affiliate_accounts WHERE referral_code = ?", [code]);
     if (!affiliate) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
     const ip = req.headers.get("x-forwarded-for") || req.headers.get("x-real-ip") || "unknown";
     const ua = req.headers.get("user-agent") || "";
-    const referrer = req.headers.get("referer") || "";
-    await recordClick({ affiliate_id: affiliate.id, ip, user_agent: ua, referrer });
-    const res = NextResponse.redirect(new URL("/", req.url));
+    const res = NextResponse.redirect(new URL("/login", req.url));
     res.cookies.set("ref_code", code, { maxAge: 60 * 60 * 24 * 30, path: "/" });
     return res;
   } catch (err) {
-    return NextResponse.json({ error: err.message, stack: err.stack }, { status: 500 });
+    const resp = NextResponse.json({ error: err.message, stack: err.stack }, { status: 500 });
+    resp.headers.set("Access-Control-Allow-Origin", "*");
+    return resp;
   }
 }
